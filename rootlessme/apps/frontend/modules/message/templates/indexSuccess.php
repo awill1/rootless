e89c7b4a5013f57d42f6a5debbc5f66d7c1be9b1
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-06-28 03:41:31 +0000

    - Updated the grid style on the inbox to look more like the new mockups

--- rootlessme/apps/frontend/modules/message/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/message/templates/indexSuccess.php
@@ -26,14 +26,6 @@
     </script>
     <table id="messageTable">
       <thead>
-    <!--    <tr>
-          <th>Message</th>
-          <th>Conversation</th>
-          <th>Author</th>
-          <th>Body</th>
-          <th>Created at</th>
-          <th>Updated at</th>
-        </tr>-->
         <tr>
             <th>
                 <input type="checkbox" name="CheckAll" value="Check All" onClick="javascript: if (this.checked) checkAll(document.messages_list_form); else uncheckAll(document.messages_list_form);">
@@ -50,21 +42,11 @@
             <tr class="<?php echo fmod($i, 2) ? 'messageListAltRow' : 'messageListRow' ?>">
                 <td><input id="entry_box_4" class="message_list_check_box" type='checkbox' name='messages_selected[]' value='5'></td>
                 <td>
-                    <div class="dateBlockLarge">
-                        <div class="dateBlockMonth">
-                            <?php echo date("M",strtotime($message->getCreatedAt())) ?>
-                        </div>
-                        <div class="dateBlockDate">
-                            <?php echo date("j",strtotime($message->getCreatedAt())) ?>
-                        </div>
-                        <div class="dateBlockTime">
-                            <?php echo date("g:i A",strtotime($message->getCreatedAt())) ?>
-                        </div>
-                    </div>
+                    <?php echo date("F j\, Y",strtotime($message->getCreatedAt())) ?>
                 </td>
                 <td>
                     <a class="messageListAuthorLink" href="<?php echo url_for("profile_show_user", $author) ?>">
-                        <img class="messageListAuthorProfileImage" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $author->getPictureUrlSmall(); ?>" alt="<?php echo $author->getFullName() ?>" />
+                        <img class="messageListAuthorProfileImage" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $author->getPictureUrlTiny(); ?>" alt="<?php echo $author->getFullName() ?>" />
                         <?php echo $author->getFullName() ?>
                     </a>
                 </td>
@@ -79,14 +61,6 @@
                     </div>
                 </td>
             </tr>
-    <!--    <tr>
-          <td><a href="<?php echo url_for('message/show?message_id='.$message->getMessageId().'&conversation_id='.$message->getConversationId()) ?>"><?php echo $message->getMessageId() ?></a></td>
-          <td><a href="<?php echo url_for('message/show?message_id='.$message->getMessageId().'&conversation_id='.$message->getConversationId()) ?>"><?php echo $message->getConversationId() ?></a></td>
-          <td><?php echo $message->getAuthorId() ?></td>
-          <td><?php echo $message->getBody() ?></td>
-          <td><?php echo $message->getCreatedAt() ?></td>
-          <td><?php echo $message->getUpdatedAt() ?></td>
-        </tr>-->
         <?php endforeach; ?>
       </tbody>
     </table>
