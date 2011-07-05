f813ad6865abf72a6129f6f92c4cf28c51c1f4e7
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-07-05 15:08:37 +0000

    - Added a new layout for the static pages

---
+++ rootlessme/apps/frontend/templates/staticLayout.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+      <title>
+          <?php if (!include_slot('title')): ?>
+            Rootless Me - Share your ride.
+          <?php endif; ?>
+      </title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php use_javascript(sfConfig::get('app_jquery_script')) ?>
+    <?php use_javascript(sfConfig::get('app_jquery_ui_script')) ?>
+    <?php use_javascript('jquery-ui-timepicker-addon.js') ?>
+    <?php use_stylesheet(sfConfig::get('app_jquery_ui_stylesheet')) ?>
+    <?php use_javascript('headerMenu') ?>
+    <?php use_javascript('navigation') ?>
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
+    <?php if (has_slot('gmapheader')): ?>
+        <?php include_slot('gmapheader') ?>
+    <?php endif; ?>
+
+  </head>
+  <body>
+      <div id="container">
+
+        <!-- Header -->
+        <div id="header">
+            <a href="<?php echo url_for('home') ?>" ><img id="headerLogo" src="/images/logo.PNG" alt="RootlessMe" /></a>
+            <div id="headerControls">
+
+                <ul id="headerControlsList">
+                    <li class="headerControlsListItem">
+                        <form action="search.php" >
+                            <input id="searchInput" />
+                        </form>
+                    </li>
+                    <li class="headerControlsListItem"><a href="#" class="headerControl">Donate</a></li>
+                    <?php if ($sf_user->isAuthenticated()): ?>
+                        <?php include_component('message', 'messageMenu') ?>
+                    <li class="headerControlsListItem">
+                        <a href="<?php echo url_for('profile_show_user',$sf_user->getGuardUser()->getPeople()->getProfiles()->getFirst()) ?>" class="headerControl">
+                            <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $sf_user->getGuardUser()->getPeople()->getProfiles()->getFirst()->getPictureUrlTiny(); ?>" alt="Tiny profile picture" />
+                            <?php echo $sf_user->getGuardUser()->getPeople(); ?>
+                            <img src="/images/menuDownArrow.JPG" alt="Profile Menu" />
+                        </a>
+                        <ul class="headerControlsListSublist">
+                            <li class="headerControlsListSublistItem">
+                                <a class="headerSublistControl" href="<?php echo url_for('profile_edit_user') ?>">
+                                    Settings
+                                </a>
+                            </li>
+                            <li class="headerControlsListSublistItem">
+                                <a class="headerSublistControl" href="<?php echo url_for('sf_guard_signout') ?>">
+                                    Sign out
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <?php else: ?>
+                    <li class="headerControlsListItem">
+                        <a class="headerControl" href="<?php echo url_for('sf_guard_signin') ?>">
+                            Log in
+                        </a>
+                    </li>
+                    <?php endif ?>
+                </ul>
+            </div>
+        </div>
+
+        <!-- End of header -->
+
+        <div id="content">
+
+            <div id="leftColumn">
+                Left column
+            </div>
+
+            <!--<div id="rightColumn"><p>This is my rightColumn</p></div> -->
+
+            <div id="middleContent">
+                    <?php echo $sf_content ?>
+            </div>
+        </div>
+        <!-- Footer -->
+        <div id="footer">
+            <hr class="footerBar" />
+            &copy;2011 Star Banana, LLC. All rights reserved.
+        </div>
+        <!-- End of footer -->
+
+    </div>
+  </body>
+</html>
