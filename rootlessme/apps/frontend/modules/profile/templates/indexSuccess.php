7be8e2249feb9c0f6b23ed226d12dc0e34b9bc41
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-07-15 01:25:59 +0000

    - Created first draft of the travelers page

--- rootlessme/apps/frontend/modules/profile/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/profile/templates/indexSuccess.php
@@ -1,76 +1,30 @@
-<h1>Profiles List</h1>
+<?php use_stylesheet('profile.css') ?>
 
-<table>
-  <thead>
-    <tr>
-      <th>Profile name</th>
-      <th>Person</th>
-      <th>First name</th>
-      <th>Last name</th>
-      <th>Picture url</th>
-      <th>Picture url large</th>
-      <th>Picture url medium</th>
-      <th>Picture url small</th>
-      <th>Picture url tiny</th>
-      <th>Address 1</th>
-      <th>Address 2</th>
-      <th>City</th>
-      <th>State</th>
-      <th>Postal code</th>
-      <th>Country</th>
-      <th>Birthday</th>
-      <th>Gender</th>
-      <th>About me</th>
-      <th>Top 5</th>
-      <th>Wants to travel to</th>
-      <th>Music</th>
-      <th>Movies</th>
-      <th>Books</th>
-      <th>Interests</th>
-      <th>Favorite websites</th>
-      <th>Website url</th>
-      <th>Facebook user name</th>
-      <th>Twitter user name</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($profiles as $profile): ?>
-    <tr>
-      <td><a href="<?php echo url_for('profile/show?profile_name='.$profile->getProfileName()) ?>"><?php echo $profile->getProfileName() ?></a></td>
-      <td><?php echo $profile->getPersonId() ?></td>
-      <td><?php echo $profile->getFirstName() ?></td>
-      <td><?php echo $profile->getLastName() ?></td>
-      <td><?php echo $profile->getPictureUrl() ?></td>
-      <td><?php echo $profile->getPictureUrlLarge() ?></td>
-      <td><?php echo $profile->getPictureUrlMedium() ?></td>
-      <td><?php echo $profile->getPictureUrlSmall() ?></td>
-      <td><?php echo $profile->getPictureUrlTiny() ?></td>
-      <td><?php echo $profile->getAddress1() ?></td>
-      <td><?php echo $profile->getAddress2() ?></td>
-      <td><?php echo $profile->getCity() ?></td>
-      <td><?php echo $profile->getState() ?></td>
-      <td><?php echo $profile->getPostalCode() ?></td>
-      <td><?php echo $profile->getCountry() ?></td>
-      <td><?php echo $profile->getBirthday() ?></td>
-      <td><?php echo $profile->getGender() ?></td>
-      <td><?php echo $profile->getAboutMe() ?></td>
-      <td><?php echo $profile->getTop5() ?></td>
-      <td><?php echo $profile->getWantsToTravelTo() ?></td>
-      <td><?php echo $profile->getMusic() ?></td>
-      <td><?php echo $profile->getMovies() ?></td>
-      <td><?php echo $profile->getBooks() ?></td>
-      <td><?php echo $profile->getInterests() ?></td>
-      <td><?php echo $profile->getFavoriteWebsites() ?></td>
-      <td><?php echo $profile->getWebsiteUrl() ?></td>
-      <td><?php echo $profile->getFacebookUserName() ?></td>
-      <td><?php echo $profile->getTwitterUserName() ?></td>
-      <td><?php echo $profile->getCreatedAt() ?></td>
-      <td><?php echo $profile->getUpdatedAt() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<h1>Travelers</h1>
 
-  <a href="<?php echo url_for('profile/new') ?>">New</a>
+<div id="middleFeaturedTraveler">
+    <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?>profile_aaron_large.jpg" alt="Aaron Williams" />
+    <div class="imageCaption">Featured Traveler: Aaron Williams</div>
+</div>
+
+<div id="middleSearchTravelers">
+    <h2>Search travelers!</h2>
+    <form class="userInputForm" action="profileSearch.php">
+        <input type="textbox" value="Who do you know?" />
+        <input type="submit" value="find" />
+    </form>
+    <ul id="travelersList">
+        <?php foreach ($profiles as $profile): ?>
+        <li class="travelersListItem">
+            <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $profile->getPictureUrlSmall() ?>" alt="<?php echo $profile->getFullName() ?>" />
+            <br />
+            <a href="<?php echo url_for('profile_show_user', array('profile_name'=>$profile->getProfileName())) ?>">
+                <?php echo $profile->getFullName() ?>
+            </a>
+            <br />
+            <?php echo $profile->getCity() ?>, <?php echo $profile->getState() ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+
