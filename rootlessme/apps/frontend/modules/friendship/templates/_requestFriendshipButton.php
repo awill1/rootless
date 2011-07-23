37b0103d4d8a953c509c5e6e8af60a66ca3c007e
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-07-23 20:06:59 +0000

    - Removed some extra javascript code in _requestFriendshipButton.php that was accidentally copied from the reviews partial. This was causing a javascript error when the person_id was not found.

--- rootlessme/apps/frontend/modules/friendship/templates/_requestFriendshipButton.php
+++ rootlessme/apps/frontend/modules/friendship/templates/_requestFriendshipButton.php
@@ -6,52 +6,12 @@
 <script type="text/javascript">
     $(document).ready(function()
     {
-        // Hide the new review form initially
-        $('#newReviewForm').hide();
-
         // Show or hide the review form as needed
         $('#addFriendButton').button().click(function(){
             // Submit a friend request
             return false;
         });
-
-        // Hide the new review temporary holder
-        $('temporaryNewReviewHolder').hide();
-
-        // New review submitting button
-        if ($('#newReviewForm').length)
-        {
-            $('#newReviewForm').ajaxForm(
-            {
-                // The resulting html should be sent to the test div
-                target: '#temporaryNewReviewHolder',
-                // The callback function when the form was successfully submitted
-                success: function() {
-                    // Move the resulting html from the temporaryNewReviewHolder
-                    // to the actual review list.
-                    $('#driverReviewsList').prepend($('#temporaryNewReviewHolder').contents());
-                    // Update the ratings graphs
-                    $('#driverRatingSummary').load(
-                        '<?php echo url_for('review_ratings', array('id' => $personID) ) ?>',
-                        { },
-                        function() {
-                            // Hide the loader and show the review graphs
-                            $('#graphLoader').hide();
-                            $('#driverRatingSummary').show();
-                            // Clear the form
-                            $('#newReviewForm').clearForm();
-                            // Show the add a review button
-                             $('#newReviewButton').show();
-                        }
-                    );
-                    // Update the review list
-                }
-            });
-        }
-
     });
-
-
 </script>
 <?php end_slot();?>
 
