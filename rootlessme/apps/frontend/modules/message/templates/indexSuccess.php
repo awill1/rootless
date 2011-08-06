494c4637ce822492df29959d1906957bdbd9f9c9
guglielmi.tony@gmail.com <guglielmi.tony@gmail.com@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-08-06 17:53:43 +0000

    changed styles to message tab

--- rootlessme/apps/frontend/modules/message/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/message/templates/indexSuccess.php
@@ -65,7 +65,7 @@
         <li class="selectedNav"><a href="<?php echo url_for('messages_list', array('list_type'=>'inbox')) ?>" title="Inbox">Inbox</a></li>
         <li class=""><a href="<?php echo url_for('messages_list', array('list_type'=>'sent')) ?>" title="Sent">Sent</a></li>
         <li class=""><a href="<?php echo url_for('messages_list', array('list_type'=>'trash')) ?>" title="Trash">Trash</a></li>
-        <li class=""><a href="<?php echo url_for('messages_new') ?>" class="messageButton" title="new_message">+ Message</a></li>
+        <li class=""><a href="<?php echo url_for('messages_new') ?>" class="messageButton" title="new_message">+ Compose</a></li>
     </ul>
 </div>
 <div id="contentBox"></div>
