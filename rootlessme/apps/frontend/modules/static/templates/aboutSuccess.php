935a41165c7e2b9a5a68abe4c01405b1495250ce
colinhoell <colinhoell@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-08-08 21:06:08 +0000

    -Styled the 'About Us' 'Safety Tips' and 'Terms of Service' pages

--- rootlessme/apps/frontend/modules/static/templates/aboutSuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/aboutSuccess.php
@@ -5,8 +5,30 @@
 
 <h1>About us</h1>
 <div>
-    <p>
-        Rootless is a new kind of social network. One that exists only
-        to help you get out from behind your computer and enjoy life.
+    <p id="footerPages">
+        Rootless.me was an idea we came up with one summer not too long ago. 
+        With gas prices high and a large population of young adults without cars, 
+        we decided it was time to change the way people travel. There are so many 
+        empty seats on the road and no real reason for it to be that way. Our solution is Rootless.me.
+        <br/>
+        <br/>
+        We started off as 3 design students at the University of Cincinnati, but quickly realized in 
+        order to get this idea off the ground we would really need some developers. We grew our 
+        operation to 5, and that is what we are today.
+        <br/>
+        <br/>
+        Really, Rootless.me is all about meeting people. It doesn't matter who you are or where 
+        you're from, you can share the road with someone else and hopefully learn something along 
+        the way. We want to use the Internet as a way to bring people closer together and interact 
+        in person, not just a place for looking at silly cat pictures (but we do that too).
+        <br/>
+        <br/>
+        If you have any questions or suggestions, please visit the 'Contact Us' page. We really 
+        appreciate any and all feedback!
+        <br/>
+        <br/>
+        Thank you,
+        <br/>
+        <span id="footerSignature">The Rootless.me Crew.</span>
     </p>
 </div>
--- rootlessme/apps/frontend/modules/static/templates/safetySuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/safetySuccess.php
@@ -5,8 +5,31 @@
 
 <h1>Safety Tips</h1>
 <div>
-    <p>
-        Get this from couch surfing.
-    </p>
+    <p id="footerPages">
+<span id="footerSignature">1.)</span>  Be selective in who you ride with. Look for users with complete profiles.
+        <br/>
+        <br/>
+<span id="footerSignature">2.)</span>  Ask for the driver's license and proof of insurance before you leave.
+        <br/>
+        <br/>
+<span id="footerSignature">3.)</span>  Confirm the phone number by calling it ahead of time. Have a conversation with <span id="footerLineBreak">your companion beforehand.</span>
+        <br/>
+        <br/>
+<span id="footerSignature">4.)</span>  Meet your ride in a public place.
+        <br/>
+        <br/>
+<span id="footerSignature">5.)</span>  Wear a seat belt!
+        <br/>
+        <br/>
+<span id="footerSignature">6.)</span>  Exchange emergency contact information.
+        <br/>
+        <br/>
+<span id="footerSignature">7.)</span>  Inform your friends and family of your trip.
+        <br/>
+        <br/>
+<span id="footerSignature">8.)</span>  Be smart. Don't take a ride if you don't feel comfortable doing so.
+        <br/>
+        <br/>
+<span id="footerSignature">9.)</span>  Have fun! The best experiences are had when you really try to get to know the <span id="footerLineBreak"> person you are riding with.</span>
 
 </div>
