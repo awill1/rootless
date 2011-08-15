cfd27173cc2a9b5bf12bb723413fbabfc72860e8
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-08-15 15:24:15 +0000

    - Changed the travelers search form to use the main site search - Cleaned up some xhtml errors on the travelers page

--- rootlessme/apps/frontend/modules/profile/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/profile/templates/indexSuccess.php
@@ -6,25 +6,25 @@
     <img src="/images/featuredTraveler.jpg" alt="Aaron Williams" />
     <div class="imageCaption">Featured Traveler <br/> <span class ="imageCaptionName"> Aaron Williams</span></div>
 </div>
-    <form class="userInputForm" action="profileSearch.php">
-        <input class="searchPeople" type="textbox" value="Search Travelers..." onblur="if (this.value == ''){this.value = 'Search Travelers...';}" onfocus="if(this.value == 'Search Travelers...'){this.value='';}"/>
-        <input class="searchSubmit" type="submit" value="find" />
-    </form>
+<form class="userInputForm" action="<?php echo url_for('search') ?>" method="get">
+    <input class="searchPeople" name="query" type="textbox" value="Search Travelers..." onblur="if (this.value == ''){this.value = 'Search Travelers...';}" onfocus="if(this.value == 'Search Travelers...'){this.value='';}"/>
+    <input class="searchSubmit" type="submit" value="find" />
+</form>
 <div class="middleSearchTravelers">
-    <div class="SearchTravelerTitle"">People You May Know</div>
+    <div class="SearchTravelerTitle">People You May Know</div>
     <div class="travelersList">
         <?php foreach ($profiles as $profile): ?>
         <div class="travelersListItem">
             <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $profile->getPictureUrlSmall() ?>"  alt="<?php echo $profile->getFullName() ?>" />
             <br />
-            <a href="<?php echo url_for('profile_show_user', array('profile_name'=>$profile->getProfileName())) ?>">
         </div>
-             <div class="travelerInformation">       
+        <div class="travelerInformation">
+            <a href="<?php echo url_for('profile_show_user', array('profile_name'=>$profile->getProfileName())) ?>">
                 <span class="travelerListName"><?php echo $profile->getFullName() ?></span>
-                
                 <span class="travelerHomeTown"><?php echo $profile->getCity() ?>, <?php echo $profile->getState() ?></span>
-            </div>
+            </a>
+        </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 </div>
 
