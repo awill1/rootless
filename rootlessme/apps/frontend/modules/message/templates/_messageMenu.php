db8317ed97e2a8509fdfbe6573cd14936365bf6f
guglielmi.tony@gmail.com <guglielmi.tony@gmail.com@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-08-20 20:26:14 +0000

    - started stying for the quick inbox view

--- rootlessme/apps/frontend/modules/message/templates/_messageMenu.php
+++ rootlessme/apps/frontend/modules/message/templates/_messageMenu.php
@@ -10,7 +10,15 @@
      <?php foreach ($newMessages as $newMessage): ?>
         <li class="headerControlsListSublistItem">
             <a class="headerSublistControl" href="<?php echo url_for('messages_show',$newMessage) ?>">
-                <?php echo $newMessage->getSubject() ?>
+                <?php 
+                    $profile = $newMessage->getPeople()->getProfiles()->getFirst();
+                    echo "<img src='/uploads/assets/profile_pictures/";
+                    echo $profile->getPictureUrlSmall();
+                    echo "' />";
+                    echo $profile->getFullName();
+                    echo $newMessage->getSubject();
+                    echo $newMessage->getBody();
+                ?>
             </a>
         </li>
     <?php endforeach; ?>
