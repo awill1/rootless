19e8ecadb55b7f0292f0b66a0b4f22fbd3b99d0a
colinhoell <colinhoell@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-09-02 15:18:57 +0000

    -Created a 'dashboard.css' file to begin styling the dashboard. -started to box all the information in

--- rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
@@ -1,4 +1,30 @@
+<?php use_stylesheet('dashboard.css') ?>
+
 <h1>Dashboard</h1>
-<p>
-    Welcome to your Rootless.Me dashboard.
-</p>
+<div id="rideSection">
+    <h3>Rides</h3>
+    <div id="rideInformationDashLeft">
+        <p>yep</p>
+    </div>
+    <div id="rideInformationDashRight">
+        <p>yep</p>
+    </div>
+</div>
+<div id="quickRidesBox">
+    <h3>Requests/Offers</h3>
+    <div id="quickBoxItem">
+        <p>yep</p>
+    </div>
+    <div id="quickBoxItem">
+        <p>yep</p>
+    </div>
+    <div id="quickBoxItem">
+        <p>yep</p>
+    </div>
+    <div id="quickBoxItem">
+        <p>yep</p>
+    </div>
+</div>
+<div id="feedSection">
+    <h3>Feed</h3>
+</div>
