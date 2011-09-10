1363bfa719f879479696985eb1fc1408716c8057
colinhoell <colinhoell@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-09-10 20:45:06 +0000

    -I finished the copy in the google doc for the footer pages and implemented it. -Fixed a color problem in the dashboard.

--- rootlessme/apps/frontend/modules/static/templates/contactSuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/contactSuccess.php
@@ -6,11 +6,32 @@
 <h1>Contact us</h1>
 <div>
     <p id="footerPages">
-        We would love to hear from you. Our email address is:
+        We would love to hear from you. Our general email address is:
 
     <br/>
+        <span id="footerSignature">Contact@rootless.me</span>
     <br/>
-        contact@rootless.me
-    </p>
+    <br/>
+        For investment opportunities, please email:
+
+    <br/>
+        <span id="footerSignature">Business@rootless.me</span>
+    <br/>
+    <br/>
+        We're always looking to improve! For suggestions, please email:
+
+    <br/>
+        <span id="footerSignature">Feedback@rootless.me</span>
+    <br/>
+    <br/>
+        For any questions about the site or our company, Star Banana, please email:
+
+    <br/>
+        <span id="footerSignature">Help@rootless.me</span>
+    <br/>
+    <br/>
+        For complaints, concerns, or to report a user, please email:
+    <br/>
+        <span id="footerSignature">Support@rootless.me</span>
 
 </div>
--- rootlessme/apps/frontend/modules/static/templates/helpSuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/helpSuccess.php
@@ -4,10 +4,71 @@
 ?>
 
 <h1>Help</h1>
-<div>
-    <p id="footerPages">
+<div id="footerPages">
+    <p>
         We want to make sharing rides easy. Please check out the topics below,
         but if you still have questions please email us at contact@rootless.me
         and we will answer your question.
     </p>
+    <br/>
+    <br/>
+    <span id="footerSignature">1.) How much of my information is public on Rootless.me?</span><br/>
+    <p>
+        As of right now, all information posted under the ‘Profile’ section is 
+        visible to all users. The information you enter in your Settings will 
+        denote if it is public or private. We hope to roll out some more privacy 
+        settings in the near future.
+    </p><br/><br/>
+        <span id="footerSignature">2.) I have a driver's license, but I'm not 18 yet. Can I still participate on Rootless.me?</span><br/>
+    <p>
+        You must be 18 years or older to use Rootless.me.  On your 18th birthday, 
+        we will be thrilled for you to get started with the rootless community!
+    </p><br/><br/>
+            <span id="footerSignature">3.) Trips which begin and end in spots I am not going show up when I search for rides, why is this?</span><br/>
+    <p>
+        The Rootless.me ride system is designed in a way to allow drivers to pick 
+        up passengers along their route. Let’s say you are driving from Ohio to 
+        Florida and someone in Georgia needs a ride to Florida as well. Since you 
+        will be passing them on your way, why not pick them up?! 
+    </p><br/><br/>
+            <span id="footerSignature">4.) Can I donate to Rootless.me?</span><br/>
+    <p>
+        As of right now, no. We are currently in beta mode, but hopefully in 
+        the near future you can give to support this grassroots site!
+        If you are interested in investment opportunities, please email Business@rootless.me
+    </p><br/><br/>
+            <span id="footerSignature">5.) I would like to delete my account from the site, how do I do that?</span><br/>
+    <p>
+        If you need to delete your account, please email us at help@rootless.me We will be 
+        rolling out a feature to delete your own account in the near future.
+    </p><br/><br/>
+            <span id="footerSignature">6.) I have suggestions for the site. Where should I send them?</span><br/>
+    <p>
+        You will find a place to leave your suggestions in your Settings, 
+        under the More Info section. You may also send us an email at 
+        feedback@rootless.me . You will find all of our contact information 
+        on our Contact Us page. We love to hear from you!
+    </p><br/><br/>
+            <span id="footerSignature">7.) Someone is harassing me through Rootless.me, what should I do?</span><br/>
+    <p>
+        Please send us a detailed description of the situation to support@rootless.me 
+        if you are being harassed by another member on the site.  We take this 
+        very seriously and will promptly take appropriate action. 
+    </p><br/><br/>
+            <span id="footerSignature">9.) How do I become a verified student?</span><br/>
+    <p>
+        You become verified student by registering with the site using your .edu email 
+        address from your college or university.  If you registered with a different 
+        email, you can change this in your Settings to become a verified student.
+    </p><br/><br/>
+            <span id="footerSignature">10.) How do I become a verified user?</span><br/>
+    <p>
+        You can become a verified member by following a few easy steps.  You first supply 
+        us with your home address and a money donation.  Once we receive an authorized 
+        donation from you, we will send a postcard to the supplied address.  You will 
+        send the postcard back to us, so that we can verify your address.  Once this 
+        is complete, other members of the site will see that you are a verified member.  
+        Start the verification process here!
+    </p><br/><br/>
+    
 </div>
