620ca6138c6773c7b7d017dc522547044a3fa353
russjwells <russjwells@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2011-09-10 22:37:56 +0000

    Dashboard ride section updates

--- rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
@@ -4,10 +4,14 @@
 <div id="rideSection">
     <h3>Rides</h3>
     <div id="rideInformationDashLeft">
-        <p>yep</p>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
     </div>
     <div id="rideInformationDashRight">
-        <p>yep</p>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
+        <div class="rideCont"><div class="dateCont"><div class="monthCont">JAN</div><div class="dayCont">1</div><div class="timeCont">9:00 AM</div></div>+ Columbus, OH <br /> <span class="viewRideLink">View Ride</span></div>
     </div>
 </div>
 <div id="quickRidesBox">
