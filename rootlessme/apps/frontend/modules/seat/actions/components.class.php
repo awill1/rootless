4d55748f4d329edc7431a49bcf7da91b36e1a1fd
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-01-12 03:48:48 +0000

    - Defaulted date and time on the seat request and offer forms to match the ride

--- rootlessme/apps/frontend/modules/seat/actions/components.class.php
+++ rootlessme/apps/frontend/modules/seat/actions/components.class.php
@@ -67,6 +67,10 @@ class seatComponents extends sfComponents
 
         // Set the default price to be the same as the ride price
         $this->seat->setPrice($this->ride->getAskingPrice());
+        
+        // Set the default pickup date and time.
+        $this->seat->setPickupDate(date("m/d/Y",strtotime($this->ride->getStartDate())));
+        $this->seat->setPickupTime(date("g:i a",strtotime($this->ride->getStartTime())));
 
         // Create the seat form
         $this->seatForm = new SeatsOfferForm($this->seat);
@@ -93,6 +97,10 @@ class seatComponents extends sfComponents
 
         // Set the default price to be the same as the ride price
         $this->seat->setPrice($this->ride->getAskingPrice());
+        
+        // Set the default pickup date. Maybe set default time.
+        $this->seat->setPickupDate(date("m/d/Y",strtotime($this->ride->getStartDate())));
+        $this->seat->setPickupTime(date("g:i a",strtotime($this->ride->getStartTime())));
 
         // Create the seat form
         $this->seatForm = new SeatsRequestForm($this->seat);
