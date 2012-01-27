4c6f0dbee0917cf2a22c769d9955fd4bf01e9a56
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-01-27 03:10:31 +0000

    - Changed dashboard to show seats under the ride posts

--- rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/dashboard/templates/indexSuccess.php
@@ -9,111 +9,88 @@
 
 <div id="rideSection">
     <ul>
-        
-    <div id="rideInformationDashLeft">
-        
-        <h3>Offers Posted</h3>
-        
-        
-        <?php foreach ($carpools as $carpool): ?>
-        <li>
-            <div class="rideCont">
-                <div class="dateCont">
-                    <div class="monthCont"><?php echo date("M",strtotime($carpool->getStartDate())) ?></div>
-                    <div class="dayCont"><?php echo date("j",strtotime($carpool->getStartDate())) ?></div>
-                    <div class="timeCont"><?php echo date("g:i A",strtotime($carpool->getStartTime())) ?></div>
-                </div>
-                <div class="locationCont">
-                +<?php echo $carpool->getOriginLocation() ?><br /> 
-                +<?php echo $carpool->getDestinationLocation() ?><br />
-                
-                
-                <a class="viewRideLink" href="<?php echo url_for('ride_show', array('ride_type' => 'offer', 'ride_id' => $carpool->getCarpoolId())) ?>" >View Ride</a>
-                </div>
-                </div>
-        </li>
-                <?php foreach ($driverSeats as $driverSeat): 
-          $passenger = $driverSeat->getPassengers()->getPeople()->getProfiles(); ?>
+        <div id="rideInformationDashLeft">
+            <h3>Offers Posted</h3>
+            <?php foreach ($carpools as $carpool): ?>
                 <li>
-        <div id="quickBoxItem">
-            <img id="leftProfilePicture" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $passenger->getPictureUrlSmall(); ?>" />
-            <p id="quickBoxName"><?php echo $passenger->getFullName() ?></p>
-            <p id="quickBoxLocationStart">+<?php echo $driverSeat->getOriginLocation() ?></p>
-            <p id="quickBoxLocationFinish">+<?php echo $driverSeat->getDestinationLocation() ?></p>
-            <p id="quickBoxConfirmButton">Confirm</p>
-            <p id="quickBoxDeclineButton">Decline</p>
-            
-            <a id="quickBoxViewRequestButton">view negotiation</a>
-            
-        </div>
-                    
+                    <div class="rideCont">
+                        <div class="dateCont">
+                            <div class="monthCont"><?php echo date("M",strtotime($carpool->getStartDate())) ?></div>
+                            <div class="dayCont"><?php echo date("j",strtotime($carpool->getStartDate())) ?></div>
+                            <div class="timeCont"><?php echo date("g:i A",strtotime($carpool->getStartTime())) ?></div>
+                        </div>
+                        <div class="locationCont">
+                            +<?php echo $carpool->getOriginLocation() ?><br /> 
+                            +<?php echo $carpool->getDestinationLocation() ?><br />
+                            <a class="viewRideLink" href="<?php echo url_for('ride_show', array('ride_type' => 'offer', 'ride_id' => $carpool->getCarpoolId())) ?>" >View Ride</a>
+                        </div>
+                    </div>
                 </li>
-           
-        <?php endforeach; ?>  
-    
-        
-                   
-        <?php endforeach; ?>
-                
-                
-                
-                
-               
-    </div>
+                <?php foreach ($driverSeats as $driverSeat): ?>
+                    <?php if ($driverSeat->getCarpoolId() == $carpool->getCarpoolId()) : ?>
+                        <?php $passenger = $driverSeat->getPassengers()->getPeople()->getProfiles(); ?>
+                        <li>
+                            <div id="quickBoxItem">
+                                <img id="leftProfilePicture" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $passenger->getPictureUrlSmall(); ?>" />
+                                <p id="quickBoxName"><?php echo $passenger->getFullName() ?></p>
+                                <p id="quickBoxLocationStart">+<?php echo $driverSeat->getOriginLocation() ?></p>
+                                <p id="quickBoxLocationFinish">+<?php echo $driverSeat->getDestinationLocation() ?></p>
+                                <p id="quickBoxConfirmButton">Confirm</p>
+                                <p id="quickBoxDeclineButton">Decline</p>
+
+                                <a id="quickBoxViewRequestButton">view negotiation</a>
+                            </div>
+                        </li>
+                   <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endforeach; ?>     
+        </div>
     </ul>
     <ul>
-    <div id="rideInformationDashRight">
-        <div class="negotiationAndPostInfoContRight">
-        <h3>Requests Posted</h3>
-        
-        <?php foreach ($passengers as $passenger): ?>
-        <li>
-            <div class="rideCont"> 
-                <div class="dateCont">
-                    <div class="monthCont"><?php echo date("M",strtotime($passenger->getStartDate())) ?></div>
-                    <div class="dayCont"><?php echo date("j",strtotime($passenger->getStartDate())) ?></div>
-                    <div class="timeCont"><?php echo date("g:i A",strtotime($passenger->getStartTime())) ?></div>
-                </div>
-                
-                
-                <div class="locationCont">
-                +<?php echo $passenger->getOriginLocation() ?><br /> 
-                +<?php echo $passenger->getDestinationLocation() ?><br /> 
-                <a class="viewRideLink" href="<?php echo url_for('ride_show', array('ride_type' => 'request', 'ride_id' => $passenger->getPassengerId())) ?>" >View Ride</a>
-                </div>
-                </div>
-            </li>
-                <?php foreach ($passengerSeats as $passengerSeat): 
-          $driver = $passengerSeat->getCarpools()->getPeople()->getProfiles(); ?>
-                
-                <li>
-        <div id="quickBoxItem">
-            <img id="leftProfilePicture" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $driver->getPictureUrlSmall(); ?>" />
-            <p id="quickBoxName"><?php echo $driver->getFullName() ?></p>
-            <p id="quickBoxLocationStart">+<?php echo $passengerSeat->getOriginLocation() ?></p>
-            <p id="quickBoxLocationFinish">+<?php echo $passengerSeat->getDestinationLocation() ?></p>
-            <p id="quickBoxConfirmButton"><a href="" >Confirm</a></p>
-            <p id="quickBoxDeclineButton"><a href="" >Decline</a></p>
-            
-            <a id="quickBoxViewRequestButton" href="<?php echo url_for('ride_show', array('ride_type'=>'offer','ride_id'=>$passengerSeat->getCarpoolId())) ?>">
-                view negotiation
-            </a>
-            
+        <div id="rideInformationDashRight">
+            <div class="negotiationAndPostInfoContRight">
+                <h3>Requests Posted</h3>
+                <?php foreach ($passengers as $passenger): ?>
+                    <li>
+                        <div class="rideCont"> 
+                            <div class="dateCont">
+                                <div class="monthCont"><?php echo date("M",strtotime($passenger->getStartDate())) ?></div>
+                                <div class="dayCont"><?php echo date("j",strtotime($passenger->getStartDate())) ?></div>
+                                <div class="timeCont"><?php echo date("g:i A",strtotime($passenger->getStartTime())) ?></div>
+                            </div>
+                            <div class="locationCont">
+                                +<?php echo $passenger->getOriginLocation() ?><br /> 
+                                +<?php echo $passenger->getDestinationLocation() ?><br /> 
+                                <a class="viewRideLink" href="<?php echo url_for('ride_show', array('ride_type' => 'request', 'ride_id' => $passenger->getPassengerId())) ?>" >View Ride</a>
+                            </div>
+                        </div>
+                    </li>
+                    <?php foreach ($passengerSeats as $passengerSeat): ?>
+                        <?php if ($passengerSeat->getPassengerId() == $passenger->getPassengerId()): ?>
+                            <?php $driver = $passengerSeat->getCarpools()->getPeople()->getProfiles(); ?>
+                            <li>
+                                <div id="quickBoxItem">
+                                    <img id="leftProfilePicture" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $driver->getPictureUrlSmall(); ?>" />
+                                    <p id="quickBoxName"><?php echo $driver->getFullName() ?></p>
+                                    <p id="quickBoxLocationStart">+<?php echo $passengerSeat->getOriginLocation() ?></p>
+                                    <p id="quickBoxLocationFinish">+<?php echo $passengerSeat->getDestinationLocation() ?></p>
+                                    <p id="quickBoxConfirmButton"><a href="" >Confirm</a></p>
+                                    <p id="quickBoxDeclineButton"><a href="" >Decline</a></p>
+
+                                    <a id="quickBoxViewRequestButton" href="<?php echo url_for('ride_show', array('ride_type'=>'offer','ride_id'=>$passengerSeat->getCarpoolId())) ?>">
+                                        view negotiation
+                                    </a>
+
+                                </div>
+                            </li>
+                        <?php endif; ?>
+                    <?php endforeach; ?> 
+                <?php endforeach; ?>
+            </div> 
         </div>
-                </li>
-                
-        
-        <?php endforeach; ?> 
-                </div> 
-        <?php endforeach; ?>
-        
-    </div>
     </ul>
 </div>
 
-
 <div id="quickRidesBox">
     <h3>Confirmed Rides</h3>
-    
-    
 </div>
