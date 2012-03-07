cdf14aeeaaba246221a61000fcd8e05e5f43b2af
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-03-07 03:36:49 +0000

    - Added the demo landing page

---
+++ rootlessme/apps/frontend/modules/static/templates/demoSuccess.php
@@ -0,0 +1,37 @@
+<?php slot(
+  'title',
+  sprintf('Rootless Me - Demo'))
+?>
+
+<div>
+    <h1>Demo Site</h1>
+    <p>
+        Our site isn't ready yet, but we are getting close. To help you see
+        what we are working on we have created this demo site. We want your 
+        input to help make rootless.me the best ridesharing community in the 
+        world. Send your feedback 
+        <a href="mailto:contact@rootless.me">contact@rootless.me</a>. Please 
+        understand this is just a demo website. Anything your create 
+        on the site can be deleted without notice. Please do not 
+        negotiate real rides with people. You will have to wait for beta.
+        We are working on fixing bugs and some small features
+        features for our launch. We have a few different demo options for you.
+    </p>
+    <h2>Semi Scripted Demo</h2>
+    <p>
+        To see the best of what we have been working on, (and avoiding some 
+        of the in progress areas) checkout this basic script.
+        <ol>
+            <li>Login using guest guest (maybe register to prevent data changes from being confusing)</li>
+            <li>Search for a ride from A to B</li>
+            <li>Compare the rides to find a best match</li>
+            <li>Ask for a seat with the best ride match</li>
+            <li>Create a ride post</li>
+            <li>Leave a review for a rider</li>
+        </ol>
+    </p>
+    <h2>Free form demo</h2>
+    <p>
+        To just play around on our site login as guest guest or register
+    </p>
+</div>
