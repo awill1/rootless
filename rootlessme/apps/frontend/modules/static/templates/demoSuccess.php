776d5da07956b1da28967d762b478d9a001eb040
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-03-19 11:44:30 +0000

    - Fixed typos in the demo

--- rootlessme/apps/frontend/modules/static/templates/demoSuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/demoSuccess.php
@@ -9,20 +9,20 @@
         Our site isn't ready yet, but we are getting close. To help you see
         what we are working on we have created this demo site. We want your 
         input to help make rootless.me the best ridesharing community in the 
-        world. Send your feedback 
+        world. Send your feedback to
         <a href="mailto:contact@rootless.me">contact@rootless.me</a>. 
     </p>
     <p>    
-        Please understand this is just a demo website. Anything your create 
+        Please understand this is just a demo website. Anything you create 
         on the site can be deleted without notice. Please do not 
         negotiate real rides with people. You will have to wait for the beta 
         release. We are working on fixing bugs and some small features
-        features for our launch. We have a two different demo options for you.
+        for our launch. We have two different demo options for you.
     </p>
-    <h2>Scripted Demo</h2>
+    <h2>Guided Demo</h2>
     <p>
         To see the best of what we have been working on, (and avoiding some 
-        of the in progress areas) check out this basic script.
+        of the in progress areas) follow these basic steps.
         <ol>
             <li>Register for an account on our 
                 <a href="<?php echo url_for('home');?>">home page</a>. It's 
@@ -38,13 +38,13 @@
                 <strong>233 South Wacker Drive, Chicago, IL</strong> on <strong>06/07/2012</strong>.</li>
             <li>Compare the rides to find a best match. Notice how rootless.me
                 searches along the route to find a match. Passengers whose 
-                origin and destination are convenient enough to a drivers 
+                origin and destination are convenient enough to a driver's 
                 route are found. Our competition requires the driver and passenger
                 to have origins and destinations close to each other to make a
                 match. 
             </li>
             <li>Ask for a seat with the best ride match. Make sure to enter the
-                exact addresses you want to be picked up 
+                exact addresses where you want to be picked up 
                 (<strong>2855 South Glass Bowl Dr., Toledo, OH</strong>) and dropped off 
                 (<strong>233 South Wacker Drive, Chicago, IL</strong>). Feel free to choose
                 a price that you think is fair. The driver in the car can
@@ -62,25 +62,25 @@
             </li>
             <li>Unfortunately, there are no rides that match your needs. 
                 Create a ride request post by clicking <strong>+ Request a 
-                ride</strong>. Create our ride using <strong>172 West Adams Street, 
+                ride</strong>. Create a ride using <strong>172 West Adams Street, 
                 Chicago, IL</strong> as the origin and <strong>3677 Kentucky 92,
                 Jamestown, KY</strong> as the destination. Hopefully a compatible
                 driver will find your ride request post and will help you get to the
                 boat house.
             </li>
             <li>
-                Imaging your ride to Chigago went very well. The driver was nice,
+                Imagine your ride to Chicago went very well. The driver was nice,
                 on time, and it was overall a really positive experience. You 
-                should review the driver so their reputation in the rootless 
+                should review the driver so their reputation in the rootless.me 
                 community will grow. Use the <strong>travelers</strong> page to
                 find the driver who you rode with. On their profile page, leave
                 a review and give them the praise they deserve.
             </li>
             <li>
-                One more thing to notice, our site works internationally. Search
+                One more thing to notice: our site works internationally. Search
                 for a ride from Munich, Germany to Paris, France. We do not have
-                any international rides in our demo database, but it is pretty
-                cool to know we can support ride sharing in other countries.
+                any international rides in our demo database, but it is important
+                to know we can support ridesharing in other countries.
             </li>
         </ol>
     </p>
