3fc6db94c616872edded1fd078bde91602dcb85f
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-04-21 15:26:59 +0000

    - Removed trash from inbox

--- rootlessme/apps/frontend/modules/message/templates/_messageTabs.php
+++ rootlessme/apps/frontend/modules/message/templates/_messageTabs.php
@@ -4,7 +4,6 @@
     <ul>
         <li class="selectedNav"><a href="<?php echo url_for('messages_list', array('list_type'=>'inbox')) ?>" title="Inbox">Inbox</a></li>
         <li class=""><a href="<?php echo url_for('messages_list', array('list_type'=>'sent')) ?>" title="Sent">Sent</a></li>
-        <li class=""><a href="<?php echo url_for('messages_list', array('list_type'=>'trash')) ?>" title="Trash">Trash</a></li>
         <li class=""><a href="<?php echo url_for('messages_new') ?>" class="messageButton" title="new_message">+ Compose</a></li>
     </ul>
 </div>
