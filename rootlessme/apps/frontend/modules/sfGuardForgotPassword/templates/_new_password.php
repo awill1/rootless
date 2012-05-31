5ca960de88f83cb948927100e3bb77971d2d147d
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-05-31 03:01:57 +0000

    - Beginnings of the forgot password functionality - Styled flash_notice and flash_error in css

---
+++ rootlessme/apps/frontend/modules/sfGuardForgotPassword/templates/_new_password.php
@@ -0,0 +1,9 @@
+<?php use_helper('I18N') ?>
+<?php echo __('Hi %first_name%', array('%first_name%' => $user->getFirstName()), 'sf_guard') ?>,
+
+<?php echo __('The password for %username% has been changed on rootless.me. If you did not request your password to be changed, please email us at contact@rootless.me',
+        array('%username%' => $user->getUsername()), 
+        'sf_guard') ?> 
+
+<?php echo __('Thanks,') ?> 
+<?php echo __('Rootless Me') ?> 
