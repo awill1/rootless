c66ab13ac5375798b44739b27d2a63eb73d109c7
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-06-02 17:23:07 +0000

    - Cleanup of search result styles - Added "no results found" messages to the main search results page.

--- rootlessme/apps/frontend/modules/search/templates/searchSuccess.php
+++ rootlessme/apps/frontend/modules/search/templates/searchSuccess.php
@@ -2,20 +2,26 @@
 
 <h1>Search Results</h1>
 
-
 <div id="travelersSection">
-<h3>Travelers</h3>
-
-<ul>
-    <?php foreach ($profiles as $profile): ?>
-        <li id ="userName"><img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $profile->getPictureUrlSmall() ?>"  alt="<?php echo $profile->getFullName() ?>" /><br/><a href="<?php echo url_for('profile_show_user', array('profile_name'=>$profile->getProfileName())) ?>"><?php echo $profile->getFullName() ?> </a></li>
-    <?php endforeach; ?>
-</ul>
-
+    <h3>Travelers</h3>
+    <div class="searchResults">
+        <?php if ($profiles->count() == 0): ?>
+            No travelers found.
+        <?php else: ?>
+            <ul class="searchResultsList">
+                <?php foreach ($profiles as $profile): ?>
+                    <li class="userName">
+                        <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $profile->getPictureUrlSmall() ?>"  alt="<?php echo $profile->getFullName() ?>" />
+                        <br/>
+                        <a href="<?php echo url_for('profile_show_user', array('profile_name'=>$profile->getProfileName())) ?>"><?php echo $profile->getFullName() ?> </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
 </div>
 
 <div id="rideSection">
-
-<h3>Rides</h3>
-
+    <h3>Rides</h3>
+    <div class="searchResults">No rides found.</div>  
 </div>
