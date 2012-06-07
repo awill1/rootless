277de8f856ab56307a56b55df2a3a7c11315a99f
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-06-07 00:58:13 +0000

    - Added addthis toolbar to ride pages to allow sharing of the ride pages.

--- rootlessme/apps/frontend/modules/ride/templates/_showRequest.php
+++ rootlessme/apps/frontend/modules/ride/templates/_showRequest.php
@@ -155,7 +155,20 @@
     <p id="mainRideInformation">
         <?php echo nl2br($passenger->getDescription()) ?>
     </p>
-
+    <div class="addThisToolBar">
+        <!-- AddThis Button BEGIN -->
+        <div class="addthis_toolbox addthis_default_style ">
+        <a class="addthis_button_preferred_1"></a>
+        <a class="addthis_button_preferred_2"></a>
+        <a class="addthis_button_preferred_3"></a>
+        <a class="addthis_button_preferred_4"></a>
+        <a class="addthis_button_compact"></a>
+        <a class="addthis_counter addthis_bubble_style"></a>
+        </div>
+        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fcfd8880ba6587c"></script>
+        <!-- AddThis Button END -->
+    </div>
 </div>
 
 <div id="rideProfileMap"></div>
