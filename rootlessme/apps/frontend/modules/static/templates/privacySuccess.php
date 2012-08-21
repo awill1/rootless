bd62e80dbae925625aeb0ffbbd62380e2b61142c
russjwells <russjwells@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-08-21 14:48:17 +0000

    Updated Rootless, Inc. Privacy Policy

--- rootlessme/apps/frontend/modules/static/templates/privacySuccess.php
+++ rootlessme/apps/frontend/modules/static/templates/privacySuccess.php
@@ -6,17 +6,41 @@
 <h1>Privacy agreement</h1>
 <div>
     <p id="footerPages">
-        We are a new company, just a few people sitting around our computers
-        trying to change the way people travel. As a result we do not have
-        a privacy policy. All of the information you enter into our site is
-        public for now. We have plans for privacy features, but as of
-        right now they are not available. Sorry for the inconvenience. We
-        value privacy and always strive to respect our users. Unfortunately,
-        we do not have the privacy features in place yet.
-        <br/>
-        <br/>
-        Please send us an email at contact@rootless.me with your concerns
-        and privacy requirements! We love hearing from people.
+Information Collection and Use<br/>
+Rootless, Inc. is the sole owner of the information collected on this site. We will not sell, share, or rent this information to others in ways different from what is disclosed in this statement. Rootless, Inc collects information from our users at several different points on our website. <br/><br/>
+Registration<br/>
+In order to use this website, a user must first complete the registration form. During registration a user is required to give their contact information (such as name and email address). This information is used to contact the user about the services on our site for which they have expressed interest. It is optional for the user to provide some demographic information, but encouraged so we can provide a more personalized experience on our site. <br/><br/>
+Contributions and Verification<br/>
+We request information from the user on our contribution form. Here a user must provide contact information (like name and address) and financial information (like credit card number, expiration date). This information is used for billing and verification purposes. <br/><br/>
+Cookies<br/>
+A cookie is a piece of data stored on the user's hard drive containing information about the user. Usage of a cookie is in no way linked to any personally identifiable information while on our site. Once the user closes their browser, the cookie simply terminates. For instance, by setting a cookie on our site, the user would not have to log in a password more than once, thereby saving time while on our site. If a user rejects the cookie, they may still use our site. The only drawback to this is that the user will be limited in some areas of our site. Cookies can also enable us to track and target the interests of our users to enhance the experience on our site. <br/><br/>
+Log Files<br/>
+We use IP addresses to analyze trends, administer the site, track user's movement, and gather broad demographic information for aggregate use. <br/><br/>
+Sharing<br/>
+We may share aggregated demographic information with our partners, but this is generally unlikely. This is not linked to any personal information that can identify any individual person. <br/><br/>
+We partner with another party to provide specific services. When the user signs up for these services, we will share names, or other contact information that is necessary for the third party to provide these services. <br/><br/>
+These parties are not allowed to use personally identifiable information except for the purpose of providing these services. <br/><br/>
+Links<br/>
+This web site contains links to other sites. Please be aware that we Rootless, Inc. are not responsible for the privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of each and every web site that collects personally identifiable information. This privacy statement applies solely to information collected by this Web site. <br/><br/>
+Security<br/>
+This website takes every precaution to protect our users' information. When users submit sensitive information via the website, your information is protected both online and off-line. <br/><br/>
+When our registration/verification/contribution forms ask users to enter sensitive information (such as credit card number), that information is encrypted and is protected with the best encryption software in the industry - SSL. While on a secure page, the lock icon on the bottom of Web browsers such as Mozilla Firefox and Microsoft Internet Explorer becomes locked, as opposed to un-locked, or open, when you are just 'surfing'.    <br/><br/>
+While we use SSL encryption to protect sensitive information online, we also do everything in our power to protect user-information off-line. All of our users' information, not just the sensitive information mentioned above, is restricted in our offices. Only employees who need the information to perform a specific job are granted access to personally identifiable information. Our employees must use password-protected screen-savers when they leave their desk. When they return, they must re-enter their password to re-gain access to your information. Furthermore, ALL employees are kept up-to-date on our security and privacy practices. As often as possible, as well as any time new policies are added, our employees are notified and/or reminded about the importance we place on privacy, and what they can do to ensure our customers' information is protected. Finally, the servers that we store personally identifiable information on are kept in a secure environment at our co-location facility. <br/><br/>
+If you have any questions about the security at our website, please contact us. <br/><br/>
+Supplementation of Information<br/>
+In order for this website to properly fulfill its obligation to our customers, it is necessary for us to supplement the information we receive with information from 3rd party sources. <br/><br/>
+Special Offers<br/>
+We send all new members a welcoming email to verify password and username. Established members will occasionally receive informational contacts. Out of respect for the privacy of our users we present the option to not receive these types of communications. Please see the choice and opt-out in the edit profile area under privacy. <br/><br/>
+Site and Service Updates<br/>
+We also send the user site and service announcement updates. Members are not able to un-subscribe from service announcements, which contain important information about the service. We communicate with the user to provide requested services and in regards to issues relating to their account via email. <br/><br/>
+Correction/Updating Personal Information<br/>
+If a user's personally identifiable information changes (such as your zip code), or if a user no longer desires our service, we will endeavor to provide a way to correct, update or remove that user's personal data provided to us. This can usually be done at the member information page or by contacting us. <br/><br/>
+Choice and Opt-out<br/>
+Users who no longer wish to receive our newsletter or materials from our partners may opt-out of receiving these communications by changing their privacy settings in the edit profile area on the site. <br/><br/>
+Users of our site are always notified when their information is being collected by any outside parties. We do this so our users can make an informed choice as to whether they should proceed with services that require an outside party, or not. <br/><br/>
+Notification of Changes<br/>
+If we decide to change our privacy policy, we will email a link to this page to our users so that they are always aware of what information we collect, how we use it, and under circumstances, if any, we disclose it. If at any point we decide to use personally identifiable information in a manner different from that stated at the time it was collected, we will notify users by way of an email. Users will have a choice as to whether or not we use their information in this different manner. We will use information in accordance with the privacy policy under which the information was collected. <br/>
+
     </p>
 
 </div>
