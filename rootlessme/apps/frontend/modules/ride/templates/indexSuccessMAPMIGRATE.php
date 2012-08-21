e7bac404d730fc59f38df8b6450047f289b589e2
guglielmi.tony@gmail.com <guglielmi.tony@gmail.com@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-08-21 07:59:21 +0000

    - continued map migration.

--- rootlessme/apps/frontend/modules/ride/templates/indexSuccessMAPMIGRATE.php
+++ rootlessme/apps/frontend/modules/ride/templates/indexSuccessMAPMIGRATE.php
@@ -9,14 +9,79 @@
 <?php slot('gmapheader'); ?>
     <script type="text/javascript" src="/js/<?php echo sfConfig::get('app_jquery_form_script') ?>"></script>
     <script type="text/javascript" src="/js/<?php echo sfConfig::get('app_jquery_block_ui_script') ?>"></script>
-    <script type="text/javascript" src="/js/RootlessMap.js"></script>
+    <script type="text/javascript" src="/js/rootlessmap.js"></script>
 
     <script type="text/javascript">   
       $(document).ready(function(){
           
+        // Form submit options used for the ajax form
+        var formAjaxOptions = 
+        {
+            target: '#results',
+            success: function()
+            {
+                // Clear the form submit pending flag
+                isFormSubmitPending = false;
+
+                // This handler function will run when the form is complete
+                $('#loader').hide();
+                $('#results').show('blind');
+
+                // Add the results to the google map
+                LoadItemsIntoGoogleMap();
+
+                // Change the hover style
+                $("#rideTable tbody tr")
+                .hover(
+                    function()
+                    {
+                        HighlightRow($(this))
+                    }
+                    ,function()
+                    {
+                        UnHighlightRow($(this))
+                    }
+                )
+                .find('td:not(:has(:checkbox, a))')
+                    .click(function () {
+                    window.location = $(this).parent().find("a").attr("href");
+                });
+            }
+        };
+          
         $( ".datePicker" ).datepicker();  
         
-        var rootlessMap = new RootlessMap('map');
+        var rootlessMapObj = rootlessmap({mapId : 'map' });
+        
+        // Handler for the find button
+        $('#rides_find').click(function() {
+            $('#loader').show();
+            $('#results').toggle('blind');
+            ClearPolylinesFromMap();
+        });
+              
+        $('#rideSearchForm').submit(function(){
+            // Set the form submit flag
+            isFormSubmitPending = true;
+                
+            // Disable the default submission. We will let AJAX do it
+            MaybeSubmitForm();
+            return false;
+        });
+        
+        
+        /**
+         * Tries to submit the form. It will only be submitted if none of the
+         * blocking flags are set.
+         */
+        function MaybeSubmitForm()
+        {            
+            // Check to make sure nothing is blocking submitting the form
+            if (canSubmitForm() && isFormSubmitPending)
+            {
+                $('#rideSearchForm').ajaxSubmit(formAjaxOptions);
+            }
+        }
         
       });
     </script>
