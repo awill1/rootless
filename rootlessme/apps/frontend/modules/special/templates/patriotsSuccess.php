39e96a675cba5a5e2a865efc04c1a8c39c7e76e0
russjwells <russjwells@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-08-31 19:10:35 +0000

    Patriots Page JQuery Update

--- rootlessme/apps/frontend/modules/special/templates/patriotsSuccess.php
+++ rootlessme/apps/frontend/modules/special/templates/patriotsSuccess.php
@@ -3,6 +3,8 @@
   'title',
   sprintf('Rootless - Patriots'))
 ?>
+<script type="text/javascript" src="/js/<?php echo sfConfig::get('app_jquery_form_script') ?>"></script>
+<script type="text/javascript" src="/js/special.js"></script>
 <script type="text/javascript">
     $(document).ready(function()
     {
@@ -10,33 +12,45 @@
     });
     
 </script>
-<div id="socialLinks">
-        <a href="http://www.twitter.com/rootlessme"><img src="/images/twitter.png" /></a>
-        <a href="http://www.facebook.com/rootlessme"><img src="/images/facebook.png" /></a>
-    </div>
+
 <div id="main">
-    
     <div id="headline">
         Ride with other Patriots fans to the next game. 
     </div>
     <div id="formBox">
-        <form action="html_form_action.asp" method="post">
+        <form id="specialForm" action="<?php echo url_for('special_event_register'); ?>" method="post">
             <div id="formButtons">
-                I want to <input type="radio" name="userType" value="drive" />Drive or <input type="radio" name="userType" value="ride" />Ride
+                <div class="boxHeader" >I want to </div>
+                
+                <input id="wantDrive" type="radio" name="userType" style="display: none;" value="drive" />
+                <label for="wantDrive" class="driveOrRide unselectedLabel">drive</label>
+                
+                <div class="orText" > or </div> 
+                
+                <input id="wantRide" type="radio" name="userType" style="display: none;" value="ride" />
+                <label for="wantRide" class="driveOrRide unselectedLabel">ride</label>
+         
+                
+                
             </div>
             <div id="formFields">
-                <input type="text" name="name" placeholder="Name"/>
-                <input type="text" name="email" placeholder="Email"/>
+                <input class="formFields" type="text" name="name" placeholder="Name"/>
+                <input class="formFields" type="text" name="email" placeholder="Email"/>
                 
-                <select name="game">
-                    <option value="game1">game1</option>
-                    <option value="game2">game2</option>
-                    <option value="game3">game3</option>
-                    <option value="game4">game4</option>
+                <select name="game" class="formFields">
+                    <option value="9/16_Patriots_vs._Cardinals">9/16 Patriots vs. Cardinals</option>
+                    <option value="10/7_Patriots_vs._Broncos">10/7 Patriots vs. Broncos </option>
+                    <option value="10/21_Patriots_vs._Jets">10/21 Patriots vs. Jets</option>
+                    <option value="11/11_Patriots_vs._Bills">11/11 Patriots vs. Bills</option>
+                    <option value="11/18_Patriots_vs._Colts">11/18 Patriots vs. Colts</option>
+                    <option value="12/10_Patriots_vs._Texans">12/10 Patriots vs. Texans</option>
+                    <option value="12/16_Patriots_vs._49ers">12/16 Patriots vs. 49ers</option>
+                    <option value="12/30_Patriots_vs._Dolphins">12/30 Patriots vs. Dolphins</option>
                 </select>
                 <br />
-                <input type="text" name="location" placeholder="Address, City, State"/>
-                <input type="submit" value="Submit" />
+                <input class="formFields" type="text" name="location" placeholder="From: Address, City, State"/>
+                <input class="formFields" type="text" name="seats" placeholder="How many spare seats?"/>
+                <input id="formSubmit" class="formFields" type="submit" value="Submit" />
             </div>
         </form>    
         <div id="formConfirmations">
@@ -44,11 +58,30 @@
                 Thanks for ridesharing with Rootless! Your potential ride matches will be emailed to you as they become available. 
                 <br /><br />
                 Share this with your friends!
+                <br /><br />
+                <div class="addThisToolBar">
+                    <!-- AddThis Button BEGIN -->
+                    <div class="addthis_toolbox addthis_default_style ">
+                    <a class="addthis_button_preferred_1"></a>
+                    <a class="addthis_button_preferred_2"></a>
+                    <a class="addthis_button_preferred_3"></a>
+                    <a class="addthis_button_preferred_4"></a>
+                    <a class="addthis_button_compact"></a>
+                    <a class="addthis_counter addthis_bubble_style"></a>
+                    </div>
+                    <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fcfd8880ba6587c"></script>
+                    <!-- AddThis Button END -->
+                </div>
             </div>
         </div>
         
         <div class="boxCopy">
             
         </div>
+    </div>
+    <div id="socialLinks">
+        <a href="http://www.twitter.com/rootlessme"><img src="/images/twitter.png" width="30" height="30"/></a>
+        <a href="http://www.facebook.com/rootlessme"><img src="/images/f_logo.png" width="30" height="30"/></a>
     </div>
 </div>
---
+++ rootlessme/web/js/special.js
@@ -0,0 +1,40 @@
+/*
+ * Constructor for radio form styling
+ * jQuery is a dependency.
+ * @param <jQuery Object> formElem Form being styled
+ */
+
+$(document).ready(function()
+{
+    $('#formFields').hide();
+    $('#formConfirmations').hide();
+    
+    $('.driveOrRide').click(function() {
+        //alert("click");
+        if($(this).siblings("label").hasClass('selectedLabel')) {
+           $(this).siblings("label").removeClass('selectedLabel');
+           $(this).siblings("label").addClass('unselectedLabel');
+        }
+        $(this).addClass('selectedLabel');
+        $(this).siblings("label").removeClass('unselectedLabel');
+        
+        $('#formFields').show('blind');
+        
+    });
+    
+//    $("#formSubmit").click(function() {
+//        //return false;
+//    });
+    //Ajax form success
+    $('#specialForm').ajaxForm( 
+        {
+            // The resulting html should be sent to the test div
+            target: '#formConirmations',
+            // The callback function when the form was successfully submitted
+            success: function() {
+                $('#formConfirmations').show('blind');  
+            }
+        });
+    
+});
+    
