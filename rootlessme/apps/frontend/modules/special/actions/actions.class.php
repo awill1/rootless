e750b9a4c9a8aaf80c28d0600dc38fea5e8a93b9
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-08-31 18:54:35 +0000

    - Added in special events register action and route

--- rootlessme/apps/frontend/modules/special/actions/actions.class.php
+++ rootlessme/apps/frontend/modules/special/actions/actions.class.php
@@ -5,18 +5,31 @@
  *
  * @package    RootlessMe
  * @subpackage special
- * @author     awilliams
+ * @author     rwells
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class specialActions extends sfActions
 {
- /**
-  * Executes patriots action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executePatriots(sfWebRequest $request)
-  {
-    
-  }
+    /**
+     * Executes patriots action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executePatriots(sfWebRequest $request)
+    {
+
+    }
+  
+    /**
+     * Executes special events register action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeRegister(sfWebRequest $request)
+    {
+        // Verify the request parameters
+        
+        
+        // Send the email to contact at rootless.me
+    }
 }
