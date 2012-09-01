a1865bb2388fe15dca15c915287d68f398ef3529
awill1 <awill1@c2e4d6b7-9cf1-4eff-a327-321b5a653c58>
2012-09-01 20:29:38 +0000

    - Added google analytics events to the patriots page

--- rootlessme/web/js/special.js
+++ rootlessme/web/js/special.js
@@ -9,9 +9,9 @@ $(document).ready(function()
     $("#backgroundImage").attr("src", "/images/patFansBackground.jpg" );
     $('#formFields').hide();
     $('#formConfirmations').hide();
-    
+
+    // Add the click handler for the drive and ride buttons
     $('.driveOrRide').click(function() {
-        //alert("click");
         if($(this).siblings("label").hasClass('selectedLabel')) {
            $(this).siblings("label").removeClass('selectedLabel');
            $(this).siblings("label").addClass('unselectedLabel');
@@ -21,23 +21,37 @@ $(document).ready(function()
         
         $('#formFields').show('blind');
         
+        // Send an event to google analytics for the type of action taken
+        _gaq.push(['_trackEvent', 'specialEvent', 'chooseRideType', $(this).attr('for')]);
+        
     });
     
-//    $("#formSubmit").click(function() {
-//        //return false;
-//    });
-//    
+    // Form field change event handler
+    $('.formFields').change(function(){
+        // Send an event to google analytics for the type of form field changed
+        _gaq.push(['_trackEvent', 'specialEvent', 'changeFormField', $(this).attr('name')]);
+    });
+    
     //Ajax form success
     $('#specialForm').validate({
-                errorLabelContainer: $(".formError")
+        errorLabelContainer: $(".formError")
     });
     $('#specialForm').ajaxForm( 
         {
-            // The resulting html should be sent to the test div
-            //target: '#formConirmations',
+            beforeSubmit: function() {
+                // Send an event to google analytics for the form submission
+                _gaq.push(['_trackEvent', 'specialEvent', 'registerSubmitted']);
+            },
+            error: function() {
+                // Send an event to google analytics for the form submission
+                _gaq.push(['_trackEvent', 'specialEvent', 'registerError']);
+            },
             // The callback function when the form was successfully submitted
             success: function() {
-                $('#formConfirmations').show('blind');  
+                $('#formConfirmations').show('blind');
+                
+                // Send an event to google analytics to show the form was submitted properly
+                _gaq.push(['_trackEvent', 'specialEvent', 'registerSuccess']);
             }
         });
     
