d8bb51f918ce1f5c0882114e97612acdf3da6a12
Russ Wells <russ@rootless.me>
2012-09-24 19:57:52 -0400

    Message Center Enhancements – changed +Compose to + Send New Message on the main page, and changed Save to Reply in the reply box & aligned the author element with the rest of the table elements.

--- rootlessme/apps/frontend/modules/message/templates/_reply.php
+++ rootlessme/apps/frontend/modules/message/templates/_reply.php
@@ -10,12 +10,12 @@
         <p>From:</p>
     </div>
         <div class="messageAuthorPictureReply">
-        <a href="<?php echo url_for("profile_show_user", $myProfile)  ?>"></a>
-           <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $myProfile->getPictureUrlTiny() ?>" alt="<?php echo $myProfile->getFullName() ?>" />
+            <a href="<?php echo url_for("profile_show_user", $myProfile)  ?>"></a>
+            <img src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $myProfile->getPictureUrlTiny() ?>" alt="<?php echo $myProfile->getFullName() ?>" />
         </div>
         <div class="messageInformationLinkReply">
             <a class="messageAuthorLink" href="<?php echo url_for("profile_show_user", $myProfile)  ?>"><?php echo $myProfile->getFullName() ?></a>
-            </div>
+        </div>
   <table class="messageReplyBox">
     <tfoot>
       <tr>
@@ -24,7 +24,7 @@
           <?php if (!$form->getObject()->isNew()): ?>
             &nbsp;<?php echo link_to('Delete', 'message/delete?message_id='.$form->getObject()->getMessageId().'&conversation_id='.$form->getObject()->getConversationId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
           <?php endif; ?>
-          <input type="submit" value="Save" />
+          <input type="submit" value="Reply" />
         </td>
       </tr>
     </tfoot>
