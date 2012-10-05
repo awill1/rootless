298bccfd177e0cb969580bd54dcb92781357f588
Aaron Williams <aaron@rootless.me>
2012-10-05 11:17:54 -0400

    - Fixed a typo in update seat text template

--- rootlessme/apps/frontend/modules/mail/templates/_updateSeatText.php
+++ rootlessme/apps/frontend/modules/mail/templates/_updateSeatText.php
@@ -2,7 +2,7 @@
       $otherUserProfile = $otherUser->getProfiles();
 ?>Hi <?php echo $subscriberProfile->getFirstName(); ?>,
 
-<?php echo $otherUserProfile->getFullName(); ?> has changes the terms of your trip from <?php echo $seat->getRoutes()->getOriginString(); ?> to <?php echo $seat->getRoutes()->getDestinationString(); ?> on <?php echo $seat->getPickupDate(); ?> at <?php echo $seat->getPickupTime(); ?>!
+<?php echo $otherUserProfile->getFullName(); ?> has changed the terms of your trip from <?php echo $seat->getRoutes()->getOriginString(); ?> to <?php echo $seat->getRoutes()->getDestinationString(); ?> on <?php echo $seat->getPickupDate(); ?> at <?php echo $seat->getPickupTime(); ?>!
 
 You can view the new terms at <?php echo url_for('ride_show', array('ride_id' => $rideId, 'ride_type' => $rideType), true).'#seat-'.$seat->getSeatId(); ?>
 
