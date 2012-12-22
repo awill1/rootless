d960b4cefd7a284e1294c5437eec55933b264433
Russ <Russ@Russ-BlackBox>
2012-12-22 17:13:02 -0500

    places index styled

--- rootlessme/apps/frontend/modules/place/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/place/templates/indexSuccess.php
@@ -1,14 +1,24 @@
-<h1>Share Rides to your Favorite Places</h1>
-
-<ul>
-    <?php foreach ($places as $place): ?>
-        <li>
-            <?php echo $place->getName() ?><br />
-            <?php echo $place->getTags() ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
-
+<?php use_stylesheet(sfConfig::get('app_css_place')) ?>
+<div id="indexMain">
+    <h1>Share rides to your favorite places</h1>
+    <ul class="placesList">
+        <?php foreach ($places as $place): ?>
+            <li>
+                <div class="placeBox">
+                    <div class="placeImg">
+                        <img src="C:\Users\Russ\rootless\ski\placesindeximg.jpg" width="361"/>
+                    </div>
+                    <div class="placeInfo">
+                        <div class='placeName'><?php echo $place->getName() ?></div>
+                        <div class='placeLocation'>City, State</div>
+                        <div class='placeTags'><?php echo $place->getTags() ?></div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<div id="devTable">
 <table>
   <thead>
     <tr>
@@ -41,5 +51,7 @@
     <?php endforeach; ?>
   </tbody>
 </table>
-
+</div>
+<div id="devNew">
   <a href="<?php echo url_for('place/new') ?>">New</a>
+</div>
