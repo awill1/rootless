fcf80d68e13aae9a2ff9983e6660f12093279e3d
Aaron Williams <aaron@rootless.me>
2012-12-22 15:44:30 -0500

    - Linked places to the routes so it can be queried and appear in the rideboard.

--- rootlessme/lib/model/doctrine/base/BaseRoutes.class.php
+++ rootlessme/lib/model/doctrine/base/BaseRoutes.class.php
@@ -17,11 +17,13 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @property string $origin_state
  * @property float $origin_latitude
  * @property float $origin_longitude
+ * @property integer $origin_place_id
  * @property string $destination_address
  * @property string $destination_city
  * @property string $destination_state
  * @property float $destination_latitude
  * @property float $destination_longitude
+ * @property integer $destination_place_id
  * @property integer $distance
  * @property integer $duration
  * @property timestamp $created_at
@@ -32,6 +34,9 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @property Doctrine_Collection $Passengers
  * @property Doctrine_Collection $Seats
  * @property Doctrine_Collection $SeatsHistory
+ * @property Places $Origin_Places
+ * @property Places $Destination_Places
+ * @property Doctrine_Collection $Places
  * 
  * @method integer             getRouteId()               Returns the current record's "route_id" value
  * @method string              getCopyright()             Returns the current record's "copyright" value
@@ -43,11 +48,13 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @method string              getOriginState()           Returns the current record's "origin_state" value
  * @method float               getOriginLatitude()        Returns the current record's "origin_latitude" value
  * @method float               getOriginLongitude()       Returns the current record's "origin_longitude" value
+ * @method integer             getOriginPlaceId()         Returns the current record's "origin_place_id" value
  * @method string              getDestinationAddress()    Returns the current record's "destination_address" value
  * @method string              getDestinationCity()       Returns the current record's "destination_city" value
  * @method string              getDestinationState()      Returns the current record's "destination_state" value
  * @method float               getDestinationLatitude()   Returns the current record's "destination_latitude" value
  * @method float               getDestinationLongitude()  Returns the current record's "destination_longitude" value
+ * @method integer             getDestinationPlaceId()    Returns the current record's "destination_place_id" value
  * @method integer             getDistance()              Returns the current record's "distance" value
  * @method integer             getDuration()              Returns the current record's "duration" value
  * @method timestamp           getCreatedAt()             Returns the current record's "created_at" value
@@ -58,6 +65,9 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @method Doctrine_Collection getPassengers()            Returns the current record's "Passengers" collection
  * @method Doctrine_Collection getSeats()                 Returns the current record's "Seats" collection
  * @method Doctrine_Collection getSeatsHistory()          Returns the current record's "SeatsHistory" collection
+ * @method Places              getOriginPlaces()          Returns the current record's "Origin_Places" value
+ * @method Places              getDestinationPlaces()     Returns the current record's "Destination_Places" value
+ * @method Doctrine_Collection getPlaces()                Returns the current record's "Places" collection
  * @method Routes              setRouteId()               Sets the current record's "route_id" value
  * @method Routes              setCopyright()             Sets the current record's "copyright" value
  * @method Routes              setSummary()               Sets the current record's "summary" value
@@ -68,11 +78,13 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @method Routes              setOriginState()           Sets the current record's "origin_state" value
  * @method Routes              setOriginLatitude()        Sets the current record's "origin_latitude" value
  * @method Routes              setOriginLongitude()       Sets the current record's "origin_longitude" value
+ * @method Routes              setOriginPlaceId()         Sets the current record's "origin_place_id" value
  * @method Routes              setDestinationAddress()    Sets the current record's "destination_address" value
  * @method Routes              setDestinationCity()       Sets the current record's "destination_city" value
  * @method Routes              setDestinationState()      Sets the current record's "destination_state" value
  * @method Routes              setDestinationLatitude()   Sets the current record's "destination_latitude" value
  * @method Routes              setDestinationLongitude()  Sets the current record's "destination_longitude" value
+ * @method Routes              setDestinationPlaceId()    Sets the current record's "destination_place_id" value
  * @method Routes              setDistance()              Sets the current record's "distance" value
  * @method Routes              setDuration()              Sets the current record's "duration" value
  * @method Routes              setCreatedAt()             Sets the current record's "created_at" value
@@ -83,6 +95,9 @@ Doctrine_Manager::getInstance()->bindComponent('Routes', 'doctrine');
  * @method Routes              setPassengers()            Sets the current record's "Passengers" collection
  * @method Routes              setSeats()                 Sets the current record's "Seats" collection
  * @method Routes              setSeatsHistory()          Sets the current record's "SeatsHistory" collection
+ * @method Routes              setOriginPlaces()          Sets the current record's "Origin_Places" value
+ * @method Routes              setDestinationPlaces()     Sets the current record's "Destination_Places" value
+ * @method Routes              setPlaces()                Sets the current record's "Places" collection
  * 
  * @package    RootlessMe
  * @subpackage model
@@ -183,6 +198,14 @@ abstract class BaseRoutes extends sfDoctrineRecord
              'autoincrement' => false,
              'length' => '',
              ));
+        $this->hasColumn('origin_place_id', 'integer', 4, array(
+             'type' => 'integer',
+             'fixed' => 0,
+             'unsigned' => false,
+             'autoincrement' => false,
+             'notnull' => false,
+             'length' => 4,
+             ));
         $this->hasColumn('destination_address', 'string', 255, array(
              'type' => 'string',
              'fixed' => 0,
@@ -227,6 +250,14 @@ abstract class BaseRoutes extends sfDoctrineRecord
              'notnull' => false,
              'autoincrement' => false,
              'length' => '',
+             ));
+        $this->hasColumn('destination_place_id', 'integer', 4, array(
+             'type' => 'integer',
+             'fixed' => 0,
+             'unsigned' => false,
+             'autoincrement' => false,
+             'notnull' => false,
+             'length' => 4,
              ));
         $this->hasColumn('distance', 'integer', 4, array(
              'type' => 'integer',
@@ -292,6 +323,18 @@ abstract class BaseRoutes extends sfDoctrineRecord
         $this->hasMany('SeatsHistory', array(
              'local' => 'route_id',
              'foreign' => 'solo_route_id'));
+
+        $this->hasOne('Places as Origin_Places', array(
+             'local' => 'origin_place_id',
+             'foreign' => 'place_id'));
+
+        $this->hasOne('Places as Destination_Places', array(
+             'local' => 'destination_place_id',
+             'foreign' => 'place_id'));
+
+        $this->hasMany('Places', array(
+             'local' => 'place_id',
+             'foreign' => 'origin_place_id'));
 
         $timestampable0 = new Doctrine_Template_Timestampable();
         $this->actAs($timestampable0);
