efc0a23d484ee70540b2b3371abf4de9bb5b4161
Timgur <guglielmi.tony@gmail.com>
2013-01-08 23:37:23 -0800

    added images to the ride search

--- rootlessme/apps/frontend/modules/ride/templates/_ridesList.php
+++ rootlessme/apps/frontend/modules/ride/templates/_ridesList.php
@@ -2,19 +2,19 @@
     <thead>
         <tr >
             <th>Date</th>
+            <th></th>
             <th>Origin</th>
             <th>&nbsp;</th>
             <th>Destination</th>
             <th>Creator</th>
-            <th>Asking Price</th>
-            <th>Type</th>
-            <th># of Seats</th>
+            <th>Price</th>
+            <th>Seats</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($carpools as $i => $carpool):
                   $route = $carpool->getRoutes(); ?>
-        <tr class="<?php echo fmod($i, 2) ? 'rideListNotSelectedAltRow' : 'rideListNotSelectedRow' ?>">
+        <tr>
             <td>
                 <div class="dateBlockLarge">
                     <div class="dateBlockMonth">
@@ -28,26 +28,30 @@
                     </div>
                 </div>
             </td>
+            <td>
+            	<span class="icon driver"></span>
+                <a class="tableLink hide" href="<?php echo url_for("ride_show",array('ride_id'=>$carpool->getCarpoolId(), 'ride_type'=>'offer')) ?>">Offer</a>
+                <span id="ride-carpool-<?php echo $carpool->getCarpoolId() ?>" class="hidden routePolyline"><?php echo $carpool->getRoutes()->getEncodedPolyline(); ?></span>
+            </td>
             <td><?php echo $route->getOriginString() ?></td>
-            <td>to</td>
+            <td><span class="icon destination-arrow"></span></td>
             <td><?php echo $route->getDestinationString() ?></td>
             <td>
                 <img class="rideListDriverCreatorProfileImage" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $carpool->getPeople()->getProfiles()->getPictureUrlSmall(); ?>" alt="<?php echo $carpool->getPeople() ?>" />
-                <?php echo $carpool->getPeople() ?>
+                <span class="ride-table-name">
+                	<?php echo $carpool->getPeople()->getProfiles()->getFirstName(); ?><br />
+                	<?php echo $carpool->getPeople()->getProfiles()->getLastName(); ?><br />
+                </span>
             </td>
             <td><?php echo '$' . $carpool->getAskingPrice() ?></td>
-            <td>
-                <a class="tableLink" href="<?php echo url_for("ride_show",array('ride_id'=>$carpool->getCarpoolId(), 'ride_type'=>'offer')) ?>">Offer</a>
-                <span id="ride-carpool-<?php echo $carpool->getCarpoolId() ?>" class="hidden routePolyline"><?php echo $carpool->getRoutes()->getEncodedPolyline(); ?></span>
-            </td>
-            <td><?php echo $carpool->getSeatsAvailable() ?></td>
+            <td style="text-align:center;"><strong><?php echo $carpool->getSeatsAvailable() ?></strong></td>
         </tr>
         <?php endforeach; ?>
         <?php foreach ($passengers as $i => $passenger): 
             $route = $passenger->getRoutes();
             // The line striping should continue from the carpools list
             $lineNumber = $i + count($carpools);?>
-        <tr class="<?php echo fmod($lineNumber, 2) ? 'rideListNotSelectedAltRow' : 'rideListNotSelectedRow' ?>">
+        <tr>
             <td>
                 <div class="dateBlockLarge">
                     <div class="dateBlockMonth">
@@ -61,19 +65,23 @@
                     </div>
                 </div>
             </td>
+            <td>
+            	<span class="icon passenger"></span>
+                <a class="tableLink hide" href="<?php echo url_for("ride_show",array('ride_id'=>$passenger->getPassengerId(), 'ride_type'=>'request')) ?>">Request</a>
+                <span id="ride-passenger-<?php echo $passenger->getPassengerId() ?>" class="hidden routePolyline"><?php echo $passenger->getRoutes()->getEncodedPolyline(); ?></span> 
+            </td>
             <td><?php echo $route->getOriginString() ?></td>
-            <td>to</td>
+            <td><span class="icon destination-arrow"></span></td>
             <td><?php echo $route->getDestinationString() ?></td>
             <td>
                 <img class="rideListDriverCreatorProfileImage" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $passenger->getPeople()->getProfiles()->getPictureUrlSmall(); ?>" alt="<?php echo $passenger->getPeople() ?>" />
-                <?php echo $passenger->getPeople() ?>
+                <span class="ride-table-name">
+                    <?php echo $passenger->getPeople()->getProfiles()->getFirstName(); ?><br />
+                    <?php echo $passenger->getPeople()->getProfiles()->getLastName(); ?>
+                </span>
             </td>
             <td><?php echo '$' . $passenger->getAskingPrice() ?></td>
-            <td>
-                <a class="tableLink" href="<?php echo url_for("ride_show",array('ride_id'=>$passenger->getPassengerId(), 'ride_type'=>'request')) ?>">Request</a>
-                <span id="ride-passenger-<?php echo $passenger->getPassengerId() ?>" class="hidden routePolyline"><?php echo $passenger->getRoutes()->getEncodedPolyline(); ?></span> 
-            </td>
-            <td><?php echo $passenger->getPassengerCount() ?></td>
+            <td  style="text-align:center;"><strong><?php echo $passenger->getPassengerCount() ?></strong></td>
         </tr>
         <?php endforeach; ?>
     </tbody>
