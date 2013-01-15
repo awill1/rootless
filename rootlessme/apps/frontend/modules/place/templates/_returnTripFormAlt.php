49c679fccfaaafe1b844690d507b68fd62f23a64
russ <russ@10.1.30.220>
2013-01-15 13:04:34 -0500

    places post a ride form styled

--- rootlessme/apps/frontend/modules/place/templates/_returnTripFormAlt.php
+++ rootlessme/apps/frontend/modules/place/templates/_returnTripFormAlt.php
@@ -7,24 +7,28 @@
     <input id="returnRouteDataInput" type="hidden" name="return_route_data" value="" />
   <div class="placeForm">
   <div class="formSection">  
-      <div id="iAmA"><span class="primaryTextStyle">I am a</span>
+      <div id="iAmA">
+          <div id="iAmAText"><span class="primaryTextStyle">I am a</span></div>
+          <div id="iAmAButtons">
             <input id="rideTypeDriver" type="radio" name="ride_type" value="driver" />
             <label for="rideTypeDriver">driver</label>
             <input id="rideTypePassenger" type="radio" name="ride_type" value="passenger" />
             <label for="rideTypePassenger">passenger</label>
             <input id="rideTypeEither" type="radio" name="ride_type" value="either" checked />
             <label for="rideTypeEither">either</label>
+          </div>
       </div>    
       <div id="startingFrom">
-            <span class="primaryTextStyle">Starting From</span>
-            <input id="originTextBox" class="longField required" type="text" name="origin" placeholder="Address, City, State" />
+          <div id="startingFromText"><span class="primaryTextStyle">Starting From</span></div>
+          <div id="startingFromInput"><input id="originTextBox" class="longField required" type="text" name="origin" placeholder="Address, City, State" /></div>
       </div>
   </div>
   <div class="formSection"><hr class="placeFormHr"></div>
   <div class="formSection">
       <div id="departDate">
-            <span class="primaryTextStyle">Depart Date</span>
-                <input id="startDateTextBox" class="datePicker shortField date" type="text" name="start_date" placeholder="MM/DD/YYYY" />
+          <div id="departDateText"><span class="primaryTextStyle">Depart Date</span></div>
+          <div id="departDateInput"><input id="startDateTextBox" class="datePicker shortField date" type="text" name="start_date" placeholder="MM/DD/YYYY" /></div>
+          <div id="departDatePicker">
                 <select id="startTimeDropDown" class="dropDown" name="start_time">
                     <option value="">Anytime</option>
                     <?php for($i = 0; $i < 24 ; $i++): ?>
@@ -32,23 +36,31 @@
                         <option value="<?php echo $i; ?>:30"><?php echo $i; ?>:30</option>
                     <?php endfor; ?>
                 </select>
-                <br />
-                <input id="startDateAnydayCheckBox" type="checkbox" name="start_date_any" />
-                <label for="startDateAnydayCheckBox"><span class='fourthTextStyle'>Any day</span></label>
+          </div>
+          <br/><br/>
+        <div id="departDateAnyDay">
+            <input id="startDateAnydayCheckBox" type="checkbox" name="start_date_any" />
+            <label for="startDateAnydayCheckBox"><span class='fourthTextStyle'>Any day</span></label>
+        </div>
       </div>
+      <br /><br />
       <div id="returnDate">
-            <span class="primaryTextStyle">Return Date</span>
-                <input id="returnDateTextBox" class="datePicker shortField date" type="text" name="return_date" placeholder="MM/DD/YYYY" />
-                <select id="returnTimeDropDown" class="dropDown" name="return_time">
+          <div id="returnDateText"><span class="primaryTextStyle">Return Date</span></div>
+          <div id="returnDateInput"><input id="returnDateTextBox" class="datePicker shortField date" type="text" name="return_date" placeholder="MM/DD/YYYY" /></div>
+          <div id="returnDatePicker">
+              <select id="returnTimeDropDown" class="dropDown" name="return_time">
                     <option value="">Anytime</option>
                     <?php for($i = 0; $i < 24 ; $i++): ?>
                         <option value="<?php echo $i; ?>:00"><?php echo $i; ?>:00</option>
                         <option value="<?php echo $i; ?>:30"><?php echo $i; ?>:30</option>
                     <?php endfor; ?>
                 </select>
-                <br />
-                <input id="returnDateAnydayCheckBox" type="checkbox" name="return_date_any" />
-                <label for="returnDateAnydayCheckBox"><span class='fourthTextStyle'>Any day</span></label>
+          </div>
+                <br /><br/>
+                <div id="returnDateAnyDay">
+                    <input id="returnDateAnydayCheckBox" type="checkbox" name="return_date_any" />
+                    <label for="returnDateAnydayCheckBox"><span class='fourthTextStyle'>Any day</span></label>
+                </div>
       </div>
   </div>   
   <div class="formSection"><hr class="placeFormHr"/></div>
@@ -56,12 +68,16 @@
         <div id="asADriver" class="secondaryTextStyle">As a driver:</div>
         <div id="seatsAvailable">    
         <span class="primaryTextStyle">Seats Available</span>
+        </div>
+        <div id="seatsAvailablePicker">
                 <select id="driverSeatsDropDown" class="tinyDropDown" name="driver_seats">
                     <?php for($i = 1; $i < 30 ; $i++): ?>
                         <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
                     <?php endfor; ?>
                 </select>
-            <span class="primaryTextStyle">Price</span>
+        </div>
+        <div id="priceText"><span class="primaryTextStyle">Price</span></div>
+        <div id="priceInput">
             <span class='dollar-sign'>$</span> <input id="driverPriceTextBox" class="tinyField number" type="text" name="driver_price" /> <span class='fourthTextStyle'>per seat</span>
         </div>   
   </div>
@@ -69,21 +85,22 @@
   <div class="formSection">
             <div id="asAPassenger" class="secondaryTextStyle">As a passenger:</div>
             <div id="seatsNeeded">
-                <span class="primaryTextStyle">Seats needed</span>
-                <select id="passengerSeatsDropDown" class="tinyDropDown" name="passenger_seats">
-                    <?php for($i = 1; $i < 30 ; $i++): ?>
-                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                    <?php endfor; ?>
-                </select>
-                <span class="primaryTextStyle">Willing to pay</span>
-                <span class='dollar-sign'>$</span> <input id="passengerPriceTextBox" class="tinyField number" type="text" name="passenger_price" /> <span class='fourthTextStyle'>per seat</span>
+                <div id="seatsNeededText"><span class="primaryTextStyle">Seats needed</span></div>
+                <div id="seatsNeededPicker">
+                    <select id="passengerSeatsDropDown" class="tinyDropDown" name="passenger_seats">
+                        <?php for($i = 1; $i < 30 ; $i++): ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                        <?php endfor; ?>
+                    </select>
+                </div>
+                <div id="willingToPayText"><span class="primaryTextStyle">Willing to pay</span></div>
+                <div id="willingToPayInput"><span class='dollar-sign'>$</span> <input id="passengerPriceTextBox" class="tinyField number" type="text" name="passenger_price" /> <span class='fourthTextStyle'>per seat</span></div>
             </div>
-        
+        <br/><br/><br/>
             <div id="otherRideDetails">
                 <span class="primaryTextStyle">Other ride details:</span><br />
                 <textarea id="otherDetailsTextArea" name="other_details" placeholder="How much space do you have/need for equipment, plans at the place, etc." ></textarea>
             </div>
-    
   </div>
           <input type="submit" class="postRideButton" value="Post Ride" />
   </div>
