0557a692b6d53f2c262c3d05801bdac08be755c4
Timgur <guglielmi.tony@gmail.com>
2013-02-10 12:53:40 -0800

    updated mustache.js to 0.7.2 and added templates to the indexSuccess.php

--- rootlessme/apps/frontend/modules/ride/templates/indexSuccess.php
+++ rootlessme/apps/frontend/modules/ride/templates/indexSuccess.php
@@ -9,6 +9,7 @@
 <?php slot('gmapheader'); ?>
     <script type="text/javascript" src="/js/map/<?php echo sfConfig::get('app_js_map_search'); ?>"></script>
     <script type="text/javascript" src="<?php echo sfConfig::get('app_jquery_form_script') ?>"></script>
+    <script type="text/javascript" src="/js/mustache.js"></script>
 
     <script type="text/javascript">   
       $(document).ready(function(){
@@ -40,3 +41,37 @@
 </div>
 <img id="loader" alt="Loading spinner" src="/images/ajax-loader.gif" style="left: 50%; margin-left: -15px; top: 0; position: relative; top: 75px;" />
 <div id="results"></div>
+<script id="rideTableTemplate" type="template/javascript">
+    <table class="rideTable">
+        <thead>
+            <tr>
+                <th><h2 class="dateHeader orange">{{startDate}}</h2></th>
+            </tr>
+    	</thead>
+        <tbody>
+        </tbody>
+    </table>
+</script>
+
+<script id="rideItemTemplate" type="template/javascript">
+    <tr>
+		<td class="person-td">
+        	<a class="tableLink hide" href="{{rideType}}"></a>
+            <img class="rideListDriverCreatorProfileImage" src="<?php echo sfConfig::get('app_profile_picture_directory') ?><?php echo $profile->getPictureUrlSmall(); ?>" alt="<?php echo $people ?>" />
+            <span class="ride-table-name">
+            	<?php echo $profile->getFirstName(); ?><br />
+                <?php echo $profile->getLastName(); ?><br />
+            </span>
+            <div class="icon <?php $rideType == 'offer'? print "driver": print "passenger" ?>"></div>
+
+            <span id="ride-<?php if($rideType == 'offer') { echo 'carpool-';} else { echo 'passenger-';} echo $id; ?>" class="hidden routePolyline"><?php echo $route->getEncodedPolyline(); ?></span>
+		</td>
+        <td class="origin-td"><?php echo $route->getOriginString(); ?></td>
+        <td><span class="icon destination-arrow"></span></td>
+        <td class="destination-td"><?php echo $route->getDestinationString(); ?></td>
+        <td>
+        	<div class="seatContainer"><?php $seats = $seats != '' ? $seats : 1; echo "<h2 class='seatCount'>" . $seats . "</h2>" . "<span class='seatText'>seat"; if($seats != 1) { echo 's'; } echo $seatText. "</span>"; ?></div>
+            <div class="cost green"><?php echo $seatCost; ?></div>
+        </td>
+    </tr>
+</script>
